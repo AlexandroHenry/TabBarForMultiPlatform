65e4c735e362421d08d1e475e3c162161e21d43f
alaxhenry <alaxhenry0121@gmail.com>
2022-11-24 13:47:24 +0900

    Initial Commit

---
+++ TabBarForMultiPlatform/TabBarForMultiPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarForMultiPlatformApp.swift
+//  TabBarForMultiPlatform
+//
+//  Created by Seungchul Ha on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TabBarForMultiPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
